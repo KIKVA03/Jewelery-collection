ca19b7a438ec1ab2f26c27ab398c97d02bf63ff8
gonjila <gonjilashvili.lasha@yahoo.com>
2022-11-20 23:44:36 +0400

    configure react router

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const HomePage = () => {
+  return <Component>sdfsdfsd</Component>;
+};
+
+export default HomePage;
+
+const Component = styled.div``;
